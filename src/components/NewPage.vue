013178a23ac73a6bb8b42a510272bbf54283110c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-05-01 01:28:46 -0700

    Replace an imported file for practice

---
+++ src/components/NewPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="helloworld">
+        {{ msg2 }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewPage",
+    props: {
+        msg: String,
+        msg2: String
+    }
+}
+</script>
